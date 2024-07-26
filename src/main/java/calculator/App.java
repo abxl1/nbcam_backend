38d02cc70c3134f28b321970c3afebdfd7648e55
abxl1 <etho_818@naver.com>
2024-07-26 17:51:12 +0900

    [CHORE] App.java 주석 ë  제거, 깃 메시지 깨짐 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,8 +5,8 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int Num1 = sc.nextInt(); // 첫 번째 양의 정수를 읽어서 저장합니다.
+        int Num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int Num2 = sc.nextInt(); // 첫 번째 양의 정수를 읽어서 저장합니다.
+        int Num2 = sc.nextInt();
     }
 }
