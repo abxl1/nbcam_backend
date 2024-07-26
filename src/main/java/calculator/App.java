d97ee45feb4c52356a2e035fb87cd2b51e233e03
abxl1 <etho_818@naver.com>
2024-07-26 18:04:15 +0900

    [FEAT] App.java add to calculator string input

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,10 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int Num1 = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String Calc = sc.nextLine();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int Num2 = sc.nextInt();
     }
