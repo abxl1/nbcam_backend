08fd45d7eb08f65691215c7cf8a8208a146bf07f
abxl1 <etho_818@naver.com>
2024-07-26 17:48:30 +0900

    [FEAT] App.java ë 양ì Scanãa©하여 양의 정수 2개(0 포함)를 전ëã달

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,12 @@
 package calculator;
+import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int Num1 = sc.nextInt(); // 첫 번째 양의 정수를 읽어서 저장합니다.
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int Num2 = sc.nextInt(); // 첫 번째 양의 정수를 읽어서 저장합니다.
     }
 }
