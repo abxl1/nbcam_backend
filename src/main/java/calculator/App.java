c74c7e429aff289d24feb77f03f8ab29ff1a3a36
abxl1 <etho_818@naver.com>
2024-07-29 16:02:21 +0900

    [FIX] App.java, Calculator.java 2-2 fix to calculating

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,10 +1,55 @@
 package calculator;
-//import java.util.ArrayList;
-//import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.calculator();
+        Scanner sc = new Scanner(System.in);
+
+//        ArrayList<Integer> arr = calculator.getArrayList(); // getter 사용
+
+
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            sc.nextLine();
+            String calc = sc.nextLine();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            int result = 0;
+
+            result = calculator.calculate(num1, num2, calc);
+
+            System.out.println("결과 : " + result);
+            calculator.arr.add(result);
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 : 삭제, enter 입력 : 계속)");
+            sc.nextLine();
+
+            if (sc.nextLine().equals("remove")) {
+                calculator.arr.remove(0);
+            }
+
+            System.out.println("저장된 연산결과를 모두 조회하시겠습니까? (inquiry 입력 : 조회, enter 입력 : 계속)");
+//            String inquiry = sc.nextLine();
+
+            if (sc.nextLine().equals("inquiry")) {
+                System.out.println(calculator.arr.toString());
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 : 종료, enter 입력 : 계속)");
+
+            if (sc.nextLine().equals("exit")) {
+                System.out.println("프로그램이 종료되었습니다.");
+                break;
+            }
+        }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,81 +1,39 @@
 package calculator;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 
 public class Calculator {
-    public void calculator() {
-        Scanner sc = new Scanner(System.in);
+    public ArrayList<Integer> arr = new ArrayList<>();
 
-        ArrayList<Integer> arr = new ArrayList<>();
+//    public ArrayList<Integer> getArrayList() { // getter 사용
+//        return arr;
+//    }
 
-        while (true) {
+    public int calculate(int num1, int num2, String calc) {
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int Num1 = sc.nextInt();
+        int result = 0;
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            sc.nextLine();
-            String Calc = sc.nextLine();
-
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int Num2 = sc.nextInt();
-
-            int result = 0;
-            boolean valid = true;
-
-            switch (Calc) {
-                case "+":
-                    result = Num1 + Num2;
-                    break;
-                case "-":
-                    result = Num1 - Num2;
-                    break;
-                case "*":
-                    result = Num1 * Num2;
-                    break;
-                case "/":
-                    if (Num2 != 0) {
-                        result = Num1 / Num2;
-                        break;
-                    } else {
-                        System.out.println("0으로 나눌 수 없습니다. 다른 숫자를 입력하세요.");
-                        valid = false;
-                        break;
-                    }
-
-                default:
-                    System.out.print("사칙연산 기호인 '+', '-', '*', '/' 중 한 가지를 입력하세요.");
-                    valid = false;
-                    break;
-            }
-
-            if (valid) {
-                System.out.println("결과 : " + result);
-                arr.add(result);
-            }
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 : 삭제, enter 입력 : 계속)");
-            sc.nextLine();
-
-            if (sc.nextLine().equals("remove")) {
-                arr.remove(0);
-//                System.out.println(arr.toString());
-            }
-
-            System.out.println("저장된 연산결과를 모두 조회하시겠습니까? (inquiry 입력 : 조회, enter 입력 : 계속)");
-//            String inquiry = sc.nextLine();
-
-            if (sc.nextLine().equals("inquiry")) {
-                System.out.println(arr.toString());
-            }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 : 종료, enter 입력 : 계속)");
-
-            if (sc.nextLine().equals("exit")) {
-                System.out.println("프로그램이 종료되었습니다.");
+        switch (calc) {
+            case "+":
+                result = num1 + num2;
                 break;
-            }
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    break;
+                }
+
+            default:
+                System.out.print("사칙연산 기호인 '+', '-', '*', '/' 중 한 가지를 입력하세요.");
+                break;
         }
+        return result;
     }
 }
