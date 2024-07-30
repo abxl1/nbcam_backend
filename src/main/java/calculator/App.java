7a5bbe8fd3c9063065d5037563ba681439381338
abxl1 <etho_818@naver.com>
2024-07-30 20:46:37 +0900

    [UPDATE] App.java Update main class to use new exception handling class

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,10 +6,11 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Exception exception = new Exception();
         Scanner sc = new Scanner(System.in);
 
         // setter가 필요한 상황 -> 컬렉션 전체를 수정해야 할 때
-        // 현재는 새 결과를 넣어 전체적인 요소의 변화는 필요가 없음 -> getter 사용
+        // 현재는 새 연산값을 넣고 첫 연산값을 빼므로 전체적인 요소의 변화는 필요가 없음
         ArrayList<Integer> arr = calculator.getArrayList(); // getter 사용
 
         while (true) {
@@ -28,6 +29,17 @@ public class App {
 
             result = calculator.calculate(num1, num2, calc);
 
+            try {
+                exception.exceptInvalidOperator(calc);
+                exception.exceptDivZero(num2, calc);
+            } catch(ArithmeticException e){ // 연산 기호를 잘못 입력한 경우
+                System.out.println("0으로 나눌 수 없습니다.");
+                throw new ArithmeticException();
+            } catch(IllegalArgumentException e) { // 0으로 나누는 경우
+             System.out.println("올바른 연산기호 (+, -, *, /)를 입력해야 합니다.");
+             throw new IllegalArgumentException();
+            }
+
             System.out.println("결과 : " + result);
             arr.add(result);
 
@@ -39,7 +51,7 @@ public class App {
             }
 
             System.out.println("저장된 연산결과를 모두 조회하시겠습니까? (inquiry 입력 : 조회, enter 입력 : 계속)");
-//            String inquiry = sc.nextLine();
+//            String inquiry = sc.nextLine(); // 반복될 연산이 복잡해졌을 경우에 변수 선언이 더 효율적
 
             if (sc.nextLine().equals("inquiry")) {
                 System.out.println(arr.toString());
