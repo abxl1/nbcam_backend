776b7d35994a080879513f25ca6c965d637ddbb3
abxl1 <etho_818@naver.com>
2024-07-31 02:05:06 +0900

    [ì´[CHORE] 2-4, 2methods for array element removal and display

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
         return arr;
     }
 
-    public int calculate(int num1, int num2, String calc) {
+    public int calculate(int num1, int num2, String calc) { // 사칙연산
 
         int result = 0;
 
@@ -25,7 +25,7 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case "/":
-                if (num2 == 0) {
+                if (num2 == 0) { // 0으로 나눌 때
                     throw new ArithmeticException("0으로 나누지 마시오.");
                     // 에러 메시지를 throw하면서 코드 진행이 종료됨.
                     // 종료시키지 않고 계속 진행시킬 수 있는가? ''' try...catch?
@@ -47,14 +47,14 @@ public class Calculator {
         }
     }
 
-    public String removeInvalid(String remove) {
+    public String removeInvalid(String remove) { // 삭제 메서드
         if (remove.equals("remove")) {
             arr.remove(0);
         }
         return remove;
     }
 
-    public String inquiryInvalid(String inquiry) {
+    public String inquiryInvalid(String inquiry) { // 조회 메서드
         if (inquiry.equals("inquiry")) {
             System.out.println(arr.toString());
         }
