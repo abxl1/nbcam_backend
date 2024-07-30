ea4626b30cf7398e8ffa459a7ada83c44c822725
abxl1 <etho_818@naver.com>
2024-07-30 20:44:23 +0900

    [UPDATE] Calculator.java Refactor exception handling

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -22,17 +22,6 @@ public class Calculator {
             case "*":
                 result = num1 * num2;
                 break;
-            case "/":
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                    break;
-                }
-
-            default:
-                System.out.print("사칙연산 기호인 '+', '-', '*', '/' 중 한 가지를 입력하세요.");
-                break;
         }
         return result;
     }
