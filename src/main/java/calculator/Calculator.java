01640d5b8a58a4a1eb894f2a33a60d728d3d239b
abxl1 <etho_818@naver.com>
2024-07-31 01:13:41 +0900

    [FIX] Calculator.java add to switch case : division zero exception

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
         return arr;
     }
 
+
     public int calculate(int num1, int num2, String calc) {
 
         int result = 0;
@@ -21,6 +22,15 @@ public class Calculator {
                 break;
             case "*":
                 result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("들어갔나요?");
+                    throw new ArithmeticException("0으로 나누지 마시오.");
+                    // 에러 메시지를 throw하면서 코드 진행이 종료됨.
+                    // 종료시키지 않고 계속 진행시킬 수 있는가? ''' try...catch?
+                }
+                result = num1 / num2;
                 break;
         }
         return result;
