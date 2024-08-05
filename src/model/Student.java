9dcf17821d4444c6d12d94c818c247551710ef3b
KangDongJoon <ehdwns1119@naver.com>
2024-08-05 16:22:57 +0900

    [FEAT/FIX] 수강생 상태 등록 기능 구현
    - CampManagmentApp클래스 displayStudent()메서드 case추가
    
    - ManagementStudent클래스 수강생 상태 등록 시스템 진입 메서드인 createStudentStatus()메서드 추가
    
    - Student클래스 수강생 상태 등록 메서드 setStudentStatus 구현

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Scanner;
+
 public class Student {
     private String studentId;   // 수강생 ID
     private String studentName; // 수강생 이름
@@ -11,6 +13,7 @@ public class Student {
         this.studentId = seq;
         this.studentName = studentName;
         this.subjects = subjects;
+        this.studentStatus = "상태미지정";
     }
 
     // Getter
@@ -26,9 +29,21 @@ public class Student {
 
     public String[] getSubjects() { return subjects; }
 
-    // Setter
-    public String setStudentStatus(String studentStatus) {
-        this.studentStatus = studentStatus;
-        return studentStatus;
+    // 수강생 상태 등록 메서드
+    public void setStudentStatus() {
+        System.out.println("수강생의 상태를 (Green, Red, Yellow)중에 입력해주세요.");
+
+        boolean vaildInput = false;
+        while(!vaildInput){
+            Scanner sc = new Scanner(System.in);
+            String input = sc.next();
+            if(input.equals("Green") || input.equals("Red") || input.equals("Yellow")){
+                studentStatus = input;
+                this.studentStatus = studentStatus;
+                vaildInput = true;
+            }else{
+                System.out.println("잘못된 상태값입니다. (Green, Red, Yellow)중에 입력해주세요.");
+            }
+        }
     }
 }
