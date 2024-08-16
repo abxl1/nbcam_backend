0c2750088a79d326f4815de559fbbec718b774fd
abxl1 <etho_818@naver.com>
2024-08-16 17:49:01 +0900

    [FEAT] add to responseDto

--- src/main/java/com/sparta/scheduler/dto/ScheduleResponseDto.java
+++ src/main/java/com/sparta/scheduler/dto/ScheduleResponseDto.java
@@ -1,11 +1,27 @@
 package com.sparta.scheduler.dto;
 
 import com.sparta.scheduler.entity.Schedule;
+import lombok.Getter;
 
+@Getter
 public class ScheduleResponseDto {
-    public ScheduleResponseDto(Schedule saveSchedule) {
+
+    private Long id;
+    private String schedule;
+    private String username;
+    private String date;
+    
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.schedule = schedule.getSchedule();
+        this.username = schedule.getUsername();
+        this.date = schedule.getDate();
     }
 
     public ScheduleResponseDto(Long id, String username, String schedule, String date) {
+        this.id = id;
+        this.username = username;
+        this.schedule = schedule;
+        this.date = date;
     }
 }
